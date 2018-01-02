8c5c964db7627c5bdeeb357ddcb2b5c0e01f3fd4
Flávio Veloso <flaviovs@magnux.com>
2018-01-01 18:48:05 -0800

    Replace tabs with spaces in source code/example
    This commit introduces no logic changes. Check with diff -w

--- jquery.exitintent.js
+++ jquery.exitintent.js
@@ -1,51 +1,51 @@
 (function ($) {
-	var timer;
+    var timer;
 
-	function trackLeave(ev) {
-		if (ev.pageY > 0) {
-			return;
-		}
+    function trackLeave(ev) {
+        if (ev.pageY > 0) {
+            return;
+        }
 
-		if (timer) {
-			clearTimeout(timer);
-		}
+        if (timer) {
+            clearTimeout(timer);
+        }
 
-		if ($.exitIntent.settings.sensitivity <= 0) {
-			$.event.trigger('exitintent');
-			return;
-		}
+        if ($.exitIntent.settings.sensitivity <= 0) {
+            $.event.trigger('exitintent');
+            return;
+        }
 
-		timer = setTimeout(
-			function() {
-				timer = null;
-				$.event.trigger('exitintent');
-			}, $.exitIntent.settings.sensitivity);
-	}
+        timer = setTimeout(
+            function() {
+                timer = null;
+                $.event.trigger('exitintent');
+            }, $.exitIntent.settings.sensitivity);
+    }
 
-	function trackEnter() {
-		if (timer) {
-			clearTimeout(timer);
-			timer = null;
-		}
-	}
+    function trackEnter() {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+    }
 
-	$.exitIntent = function(enable, options) {
-		$.exitIntent.settings = $.extend($.exitIntent.settings, options);
+    $.exitIntent = function(enable, options) {
+        $.exitIntent.settings = $.extend($.exitIntent.settings, options);
 
-		if (enable == 'enable') {
-			$(window).mouseleave(trackLeave);
-			$(window).mouseenter(trackEnter);
-		} else if (enable == 'disable') {
-			trackEnter(); // Turn off any outstanding timer
-			$(window).unbind('mouseleave', trackLeave);
-			$(window).unbind('mouseenter', trackEnter);
-		} else {
-			throw "Invalid parameter to jQuery.exitIntent -- should be 'enable'/'disable'";
-		}
-	}
+        if (enable == 'enable') {
+            $(window).mouseleave(trackLeave);
+            $(window).mouseenter(trackEnter);
+        } else if (enable == 'disable') {
+            trackEnter(); // Turn off any outstanding timer
+            $(window).unbind('mouseleave', trackLeave);
+            $(window).unbind('mouseenter', trackEnter);
+        } else {
+            throw "Invalid parameter to jQuery.exitIntent -- should be 'enable'/'disable'";
+        }
+    }
 
-	$.exitIntent.settings = {
-		'sensitivity': 300
-	};
+    $.exitIntent.settings = {
+        'sensitivity': 300
+    };
 
 })(jQuery);
